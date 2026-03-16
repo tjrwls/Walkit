90b0917fc747b6ef9979578b8bb4e2b105a0e115
seokjin <whtjrwls9895@gmail.com>
2026-03-17 00:08:52 +0900

    test: add unit and ui test targets

---
+++ WalkItTests/WalkItTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalkItTests.swift
+//  WalkItTests
+//
+//  Created by 조석진 on 12/5/25.
+//
+
+import Testing
+@testable import WalkIt
+
+struct WalkItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
